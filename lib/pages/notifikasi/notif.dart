176e3edf949b8b73ac9c1a3f651fd1d0d62876b8
AlbireoFinoe <albireo.m7@gmail.com>
2024-04-09 20:03:37 +0700

    pembayaran

--- lib/pages/notifikasi/notif.dart
+++ lib/pages/notifikasi/notif.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taxi_app/pages/pembayaran/pembayaran3.dart';
 
 class notif extends StatefulWidget {
   const notif({super.key});
@@ -26,6 +27,26 @@ class _notifState extends State<notif> {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => pembayaran3()));
+              },
+              child: Text(
+                "Carikan Ahli",
+                style: TextStyle(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff173b34),
+                  minimumSize: Size(double.infinity, 50)),
+            ),
+          ],
+        ),
       ),
     );
   }
